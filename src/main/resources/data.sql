5a6f55fa75f8e0c8b42c811508be40a55471fc17
LouisNgatale <louisngatale@gmail.com>
2021-04-24 21:49:11 +0300

    FLOORS SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,74 @@
-insert into hostel (hostel) values ('BLOCK 1');
-insert into hostel (hostel) values ('BLOCK 2');
-insert into hostel (hostel) values ('BLOCK 3');
-insert into hostel (hostel) values ('BLOCK 4');
-insert into hostel (hostel) values ('BLOCK 5');
+# INSERT HOSTELS
+INSERT INTO hostel (hostel) VALUES ('BLOCK 1'); #1
+INSERT INTO hostel (hostel) VALUES ('BLOCK 2'); #2
+INSERT INTO hostel (hostel) VALUES ('BLOCK 3'); #3
+INSERT INTO hostel (hostel) VALUES ('BLOCK 4'); #4
+INSERT INTO hostel (hostel) VALUES ('BLOCK 5'); #5
 
 # INSERT WINGS
-insert into wing (wing,hostel_id) values ('WING A',1);
-insert into wing (wing,hostel_id) values ('WING B',1);
-insert into wing (wing,hostel_id) values ('WING A',2);
-insert into wing (wing,hostel_id) values ('WING B',2);
-insert into wing (wing,hostel_id) values ('WING A',3);
-insert into wing (wing,hostel_id) values ('WING B',3);
-insert into wing (wing,hostel_id) values ('WING A',4);
-insert into wing (wing,hostel_id) values ('WING B',4);
-insert into wing (wing,hostel_id) values ('WING A',5);
-insert into wing (wing,hostel_id) values ('WING B',6);
+# BLOCK 1
+INSERT INTO wing (wing,hostel_id) VALUES ('WING A',1); #1
+INSERT INTO wing (wing,hostel_id) VALUES ('WING B',1); #2
+# BLOCK 2
+INSERT INTO wing (wing,hostel_id) VALUES ('WING A',2); #3
+INSERT INTO wing (wing,hostel_id) VALUES ('WING B',2); #4
+# BLOCK 3
+INSERT INTO wing (wing,hostel_id) VALUES ('WING A',3); #5
+INSERT INTO wing (wing,hostel_id) VALUES ('WING B',3); #6
+# BLOCK 4
+INSERT INTO wing (wing,hostel_id) VALUES ('WING A',4); #7
+INSERT INTO wing (wing,hostel_id) VALUES ('WING B',4); #8
+# BLOCK 5
+INSERT INTO wing (wing,hostel_id) VALUES ('WING A',5); #9
+INSERT INTO wing (wing,hostel_id) VALUES ('WING B',6); #10
 
+# INSERT FLOORS
+# BLOCK 1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 1',1); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 1',2); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 2',1); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 2',2); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 3',1); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 3',2); #1
 
+# BLOCK 2
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 1',3); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 1',4); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 2',3); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 2',4); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 3',3); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 3',4); #1
+
+# BLOCK 3
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 1',5); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 1',6); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 2',5); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 2',6); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 3',5); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 3',6); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 4',5); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 4',6); #1
+
+# BLOCK 4
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 1',7); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 1',8); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 2',7); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 2',8); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 3',7); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 3',8); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 4',7); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 4',8); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 5',7); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 5',8); #1
+
+# BLOCK 5
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 1',9); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 1',10); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 2',9); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 2',10); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 3',9); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 3',10); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 4',9); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 4',10); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 5',9); #1
+INSERT INTO FLOOR (floor,wing_id) VALUES ('FLOOR 5',10); #1
